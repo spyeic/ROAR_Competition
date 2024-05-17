ead3d574f4f1bd0c1a1ae7989d462af478590f43
cctz123 <162527316+cctz123@users.noreply.github.com>
2024-05-17 02:28:49 -0400

    Update submission.py

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -56,8 +56,6 @@ class RoarCompetitionSolution:
         self.f = open('race.txt', 'w')
         self.target_waypoint_idx = 5
         self.num_steps = 0
-
-
     
     async def initialize(self) -> None:
         # TODO: You can do some initial computation here if you want to.
@@ -79,8 +77,6 @@ class RoarCompetitionSolution:
         self.prior_steer_error = -2
         self.prior_steer_integral = 0
         self.target_waypoint_idx = 5
-        self.num_steps = 0
-
 
     async def step(
         self
@@ -98,10 +94,13 @@ class RoarCompetitionSolution:
         vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
         vehicle_velocity_norm = np.linalg.norm(vehicle_velocity)
 
+        # debugging controls
         show_location=False
-        show_velocity=False
+        show_velocity=True
         show_steer=False
         write_step=False
+        slowmo_start = 1365
+        slowmo_end = 1350
         self.num_steps += 1
 
         # Find the waypoint closest to the vehicle
@@ -119,10 +118,8 @@ class RoarCompetitionSolution:
             f'{self.maneuverable_waypoints[self.current_waypoint_idx].location[2]:.2f}]')
 
         ## Step 1: find the target line and the angle to the target line
-        # print(f'before: {self.target_waypoint_idx}')
         target_line, self.target_waypoint_idx = self.get_target_line(vehicle_location, self.current_waypoint_idx, self.target_waypoint_idx, self.maneuverable_waypoints)
         target_steer = np.arctan2(target_line[1],target_line[0])
-        # print(f'after: {self.target_waypoint_idx}')
 
         ## Step 2: set the steering control to go to the target line
 
@@ -130,24 +127,28 @@ class RoarCompetitionSolution:
         # vehicle_rotation is roll, pitch, yaw.  we just want the yaw so we use [2]
         current_steer = vehicle_rotation[2]
         steer_error = normalize_rad(target_steer - current_steer) / np.pi
-        steer_control = self.steer_pid_controller.run(steer_error, vehicle_velocity_norm)
+        steer_control = self.steer_pid_controller.run(steer_error, vehicle_velocity_norm, current_steer, target_steer, 
+            self.maneuverable_waypoints[self.current_waypoint_idx], self.current_waypoint_idx)
 
         # print steer
         if (show_steer):
             print(f"Error={steer_error:.3F}, Steer={steer_control:.3F}", 
                   f"Velocity={vehicle_velocity_norm:.3F}, Waypoint={self.current_waypoint_idx:}")
 
+        # slow down in slowmo range to see what is going on
+        if (slowmo_start <= self.current_waypoint_idx <= slowmo_end): sleep(0.25)
 
         # Step 3: Pick a target speed
         current_speed = vehicle_velocity_norm
-        target_speed = self.get_target_speed(vehicle_location, self.current_waypoint_idx, self.maneuverable_waypoints)
+        target_speed = self.get_target_speed(vehicle_location, self.current_waypoint_idx, self.target_waypoint_idx, self.maneuverable_waypoints)
 
         # Step 4: Set the throttle
         speed_error = target_speed - current_speed
         throttle_control = self.throttle_pid_controller.run(target_speed, speed_error, vehicle_velocity_norm)
 
         if (show_velocity):
-            print(f"Throttle/Velocity= {throttle_control:.3F}, {vehicle_velocity_norm:.3F}, Waypoint={self.current_waypoint_idx:}, Target={self.target_waypoint_idx}")
+            print(f"Throttle/Velocity= {throttle_control:.3F}, {vehicle_velocity_norm:.3F}, Waypoint={self.current_waypoint_idx:},",
+                  f"Target={self.target_waypoint_idx}, Distance={(self.target_waypoint_idx - self.current_waypoint_idx)%len(self.maneuverable_waypoints)}")
 
         if (write_step):
             self.f.write(f'{self.num_steps}, ')
@@ -190,13 +191,12 @@ class RoarCompetitionSolution:
             return (waypoint_to_follow.location - vehicle_location)[:2], (current_idx + 5) % len(waypoints)
 
         elif (algo==2):
-            # This version looks for a waypoint as far ahead as possible to find a line that does not hit a wall, limited to some maximum
-            # It does this by succeesively testing waypoints further and further out, finding the smallest to the left and right walls,
-            # and ensuring the angle to the waypoint is not hitting any of those walls.  
-            # There were 3 adjustments that had to be made:
-            # 1) you have to start a few waypoints ahead in your testing, otherwise the direction could be completely off
-            # 2) the lookahead waypoint should never go backwards, as that can cause odd steering angles
-            # 3) if we're going through an S-curve, get within 5 waypoints of the where the curve changes before sarting to look ahead again
+            # This version looks for a waypoint as far ahead as possible between both a minimum and maximum value, taking into 
+            # account three things:
+            # 1) The line to that waypoint does not hit a wall, so the angle to the line does not overlap with the angles to the
+            # left and right walls of all prior waypoints
+            # 2) Once it locks in on a waypoint, it should never pick a closer waypoint, as that can cause odd steering angles
+            # 3) if we're going through an S-curve, then don't go past the inflection point in the S-curve where it changes direction
             # Future potential improvements
             # - widen safety to 3 or 4 as steering gets better
             # - increase max_lookahead to 100 or more
@@ -205,7 +205,7 @@ class RoarCompetitionSolution:
 
             # parameters to tweak
             min_lookahead = 5
-            max_lookahead = 75
+            max_lookahead = 100
             safety = 2
 
             # initialize variables
@@ -250,7 +250,7 @@ class RoarCompetitionSolution:
 
                 # if angle to the waypoint hits a wall, then stop
                 if (angle_to_waypoint < min_right_angle or angle_to_waypoint > min_left_angle):
-                    break;
+                    break
 
                 # if direction is changed, then stop.  -1 means right, 1 means left, 0 means neutral
                 if (i==start_waypoint):
@@ -280,25 +280,49 @@ class RoarCompetitionSolution:
 
 
     # this current version simply sets the speed depending on where you are on the track
-    def get_target_speed(self, vehicle_location: np.ndarray, current_idx: int , waypoints : List[roar_py_interface.RoarPyWaypoint]):
+    def get_target_speed(self, vehicle_location: np.ndarray, current_idx: int, target_idx: int, waypoints : List[roar_py_interface.RoarPyWaypoint]):
 
-        # for now, set speed depending on waypoint so we can at least finish the race
-        if (240 < self.current_waypoint_idx < 400):  # straightaway 
-            target_speed = 25
-        elif (450 < self.current_waypoint_idx < 540):  # slight curve
-            target_speed = 15
-        elif (900 < self.current_waypoint_idx < 1200):
-            target_speed = 35
-        elif (1300 < self.current_waypoint_idx < 1500):  # sharp S curve
-            target_speed = 20
-        elif (1500 < self.current_waypoint_idx < 1800):  # straightaway
-            target_speed = 40
-        elif (2100 < self.current_waypoint_idx < 2500):
-            target_speed = 30
-        elif (2600 < self.current_waypoint_idx < 2800):   # sharp S curve
-            target_speed = 15
-        else:
-            target_speed = 20
+        # normalize waypoint
+        waypoint = self.current_waypoint_idx%len(waypoints) 
+        algo = 2
+
+        if (algo==1):
+            # set speed depending on where we are on the track
+            if (240 < waypoint < 400):  # straightaway 
+                target_speed = 75
+            elif (450 < waypoint < 540):  # slight curve
+                target_speed = 75
+            elif (900 < waypoint < 1200):
+                target_speed = 35
+            elif (1300 < waypoint < 1500):  # sharp S curve
+                target_speed = 20
+            elif (1500 < waypoint < 1800):  # straightaway
+                target_speed = 20
+            elif (2100 < waypoint < 2500):
+                target_speed = 20
+            else:
+                target_speed = 50
+
+        elif (algo==2):
+            distance = (target_idx-current_idx)%len(waypoints)
+            if (distance > 50):
+                target_speed = 300
+            elif (distance > 40):
+                target_speed = 65
+            elif (distance > 35):
+                target_speed = 45
+            elif (distance > 30):
+                target_speed = 35
+            elif (distance > 20):
+                target_speed = 25
+            elif (distance > 10):
+                target_speed = 20
+            else:
+                target_speed = 20
+
+            # special handling for the two sharp S curve until we learn how fast the car can slow down
+            if (2600 < waypoint < 2800) or (1360 < waypoint < 1450):
+                target_speed = 15
 
         return target_speed
     
@@ -307,55 +331,110 @@ class RoarCompetitionSolution:
 # PID controller to set the steering control given an error
 class SteeringPIDController():
     def __init__(self):
+        self.steer_integral = 0
         self.prior_steer_error = -2
+        self.prior_prior_steer_error = -2
         self.prior_steer_integral = 0
+        self.maxerror = -10.0
+        self.minerror = 10.0
+        self.maxovershoot = 0.0
+        self.outputed = False
+        self.logresults = False
 
+        if (self.logresults):
+            self.f = open('steeringPID.txt', 'w')
+            self.f.write('Waypoint, ')
+            self.f.write('Velocity, ')
+            self.f.write('Waypoint Angle, ')
+            self.f.write('Target Steer, ')
+            self.f.write('Current Steer, ')
+            self.f.write('Steer Error, ')
+            self.f.write('Steer Control, ')
+            self.f.write('PID, ')
+            self.f.write('Steer Error, Steer Integral, Steer Deriv, ')
+            self.f.write('KP, KI, KD')
+            self.f.write('\n')
 
-    def run(self, steer_error:float, velocity:float) -> float:
+    def run(self, steer_error:float, velocity:float, current_steer, target_steer, waypoint:roar_py_interface.RoarPyWaypoint, current_waypoint_idx) -> float:
         steer_derivative = 0
-        steer_integral = 0
         steer_kp = 0
         steer_kd = 0
         steer_ki = 0
         steer_control = float(0)
-
         if (self.prior_steer_error) != -2:
             steer_derivative = steer_error - self.prior_steer_error
-            steer_integral += steer_error
-        self.prior_steer_error = steer_error
-        self.prior_steer_integral = steer_integral
+            self.steer_integral += steer_error
+
+        # multiple solutions are coded below.  The variable algo determines which solution to use
+        # debug controls whether messages are displayed as it is working
+        algo = 2
+        debug = False
 
         # set kp, kd, and ki
-        option = 2
-        if (option == 1):
+        if (algo == 1):
             steer_kp = 4.0 / np.sqrt(velocity)
             steer_kd = 8.0 / (velocity/2)
             steer_ki = 0.002
         else:
+            # values determined through trial and error
             if (velocity < 10):
-                steer_kp = 1
-                steer_kd = 1
-            if (velocity < 25):
-                steer_kp = 0.75
-                steer_kd = 1
-            elif (velocity < 50):
-                steer_kp = 0.0
-                steer_kd = 2
-            else:
-                steer_kp = 0.25
+                steer_kp = 1.25
+                steer_kd = 3
+            elif (velocity < 20):
+                steer_kp = 3.0
                 steer_kd = 0.5
-            steer_ki = 0.005
-
-        # debug code:
-        # xP = (steer_kp * steer_error)
-        # xI = (steer_ki * steer_integral) 
-        # xD = (steer_kd * steer_derivative) 
-        # print (f"xP={xP:0.2F}, xI={xI:0.2F},xD={xD:0.2F}")
+            elif (velocity < 30):
+                steer_kp = 4.0
+                steer_kd = 1.0
+            else:
+                steer_kp = 4
+                steer_kd = 1.0
+            steer_ki = 0.000
 
         if (velocity <= 1e-2):
             steer_control = -np.sign(steer_error)
         else:
-            steer_control = -(steer_kp * steer_error) - (steer_ki * steer_integral) - (steer_kd * steer_derivative) 
+            steer_control = -(steer_kp * steer_error) - (steer_ki * self.steer_integral) - (steer_kd * steer_derivative) 
+
+        # debug code
+        if (self.logresults):
+            xP = (steer_kp * steer_error)
+            xI = (steer_ki * self.steer_integral) 
+            xD = (steer_kd * steer_derivative) 
+            self.f.write(f'{current_waypoint_idx:.2F}, ')
+            self.f.write(f'{velocity:.2F}, ')
+            self.f.write(f'{math.degrees(waypoint.roll_pitch_yaw[2]):.1F}, ')
+            self.f.write(f'{math.degrees(target_steer):.1F}, ')
+            self.f.write(f'{math.degrees(current_steer):.1F}, ')
+            self.f.write(f'{steer_error:.3F}, ')
+            self.f.write(f'{steer_control:.3F}, ')
+            self.f.write(f'PID=({xP:.3F}/{xI:.3F}/{xD:.3F}), ')
+            self.f.write(f'{steer_error:.3F}, {self.steer_integral:.3F}, {steer_derivative:.3F}, ')
+            self.f.write(f'{steer_kp:.2F}, {steer_ki:.3F}, {steer_kd:.2F} ')
+            self.f.write('\n')
+
+        # debug code for testing kp and kd combinations
+        if (debug):
+            if (current_waypoint_idx != 2774 and steer_error > self.maxerror): self.maxerror = steer_error
+            if (current_waypoint_idx != 2774 and steer_error < self.minerror): self.minerror = steer_error
+            if (self.prior_prior_steer_error != -2):
+                if ((steer_error > self.prior_steer_error) and (self.prior_steer_error < self.prior_prior_steer_error)) \
+                    or ((steer_error < self.prior_steer_error) and (self.prior_steer_error > self.prior_prior_steer_error)) :
+                    if (self.maxovershoot < abs(steer_error - self.prior_steer_error)):
+                        self.maxovershoot = abs(steer_error - self.prior_steer_error)
+
+            if (current_waypoint_idx == 500) and (not self.outputed):
+                with open("testresults.txt", "a") as f:
+                    f.write(f'{velocity:.2F}, ')
+                    f.write(f'{steer_kp:.2F}, {steer_kd:.2F}, ')
+                    f.write(f'{steer_error:.3F}, {self.minerror:.3F}, {self.maxerror:.3F}, ')
+                    f.write(f'{self.maxovershoot:.3F}, {current_waypoint_idx:}')
+                    f.write('\n')
+                self.outputed = True
+
+        self.prior_prior_steer_error = self.prior_steer_error
+        self.prior_steer_error = steer_error
+        self.prior_steer_integral = self.steer_integral
 
         return np.clip(steer_control, -1.0, 1.0) 
 
@@ -376,9 +455,9 @@ class ThrottlePIDController():
         if algo == 2:
             target_velocity=target_speed
             if velocity < target_velocity:
-                throttle_control = 2*(target_velocity-velocity)/target_velocity
+                throttle_control = 3*(target_velocity-velocity)/target_velocity
             elif velocity > target_velocity:
-                throttle_control = 2*(target_velocity-velocity)/target_velocity
+                throttle_control = 3*(target_velocity-velocity)/target_velocity
             else:
                 throttle_control = 0
         
